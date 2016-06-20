0996e604fac17e6ff3762be95cb4b869d1bd080b
Tallak Tveide <tallak@tveide.net>
2016-06-20 11:11:35 +0200

    xxSplit into two modules

--- lib/foo_lib.ex
+++ lib/foo_lib.ex
@@ -1,11 +1,15 @@
-defmodule FooLib do
+defmodule FooLibHelper do
   @params ~w(A/B B/A A B)
 
   for name <- @params do
     def foo(x, unquote(name |> String.to_atom)), do: unquote(name) <> "_" <> x
   end
 
-  def wrapper(x, y), do: foo(x, String.to_existing_atom(y))
-
   def say_ok, do: :ok
+end
+
+defmodule FooLib do
+  def wrapper(x, y), do: FooLibHelper.foo(x, String.to_existing_atom(y))
+
+  def say_ok, do: FooLibHelper.say_ok
 end
